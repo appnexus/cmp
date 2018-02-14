00dfe78b6da5899327a313d52bf7c3ce97827ec1
Kris Hoyt <khoyt@appnexus.com>
2018-02-14 08:12:52 -0800

    Add Heap analyitcs back into portal

--- src/docs/assets/portal.js
+++ src/docs/assets/portal.js
@@ -19,6 +19,41 @@ const readVendorListPromise = fetch('./vendors.json', {
 		log.error(`Failed to load vendor list from vendors.json`, err);
 	});
 
+function consentSubmitted({vendorConsentData = {}, cookieValue, cmpVersion}) {
+	const {
+		cmpId,
+		cookieVersion,
+		created,
+		lastUpdated,
+		publisherPurposeVersion,
+		selectedPurposeIds = new Set(),
+		selectedVendorIds = new Set(),
+		vendorList = {}
+	} = vendorConsentData;
+
+	const {
+		purposes = [],
+		vendors = [],
+		version,
+	} = vendorList;
+
+	window.heap && window.heap.track('Consent Submitted', {
+		cmpId,
+		cookieVersion,
+		created: created && created.getTime(),
+		lastUpdated: lastUpdated && lastUpdated.getTime(),
+		publisherPurposeVersion,
+		vendorListVersion: version,
+		totalPurposeCount: purposes.length,
+		consentedPurposeCount: selectedPurposeIds.size,
+		totalVendorCount: vendors.length,
+		consentedVendorCount: selectedVendorIds.size,
+		acceptAll: purposes.length === selectedPurposeIds.size && vendors.length === selectedVendorIds.size,
+		cookieValue,
+		cmpVersion
+	});
+}
+
 function readCookie(name) {
 	const value = '; ' + document.cookie;
 	const parts = value.split('; ' + name + '=');
@@ -40,7 +75,8 @@ const commands = {
 		return readCookie(COOKIE_NAME);
 	},
 
-	writeVendorConsent: ({encodedValue }) => {
+	writeVendorConsent: ({encodedValue, vendorConsentData, cmpVersion  }) => {
+		consentSubmitted({vendorConsentData, cookieValue: encodedValue, cmpVersion});
 		return writeCookie({name: COOKIE_NAME, value: encodedValue});
 	}
 };
