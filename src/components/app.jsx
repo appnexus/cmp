f7ca63ecec785ef1584390f791213bf63390aed7
Marco Pajares Silva <marco.pajares@smaato.com>
2018-03-06 14:22:07 +0100

    AT-633 Xhr implemented

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import { h, Component } from 'preact';
 import style from './app.less';
-
+import { readVendorConsentCookie, readPublisherConsentCookie } from '../lib/cookie/cookie';
+import { sendPortalCommand } from '../lib/portal';
 import Popup from './popup/popup';
 
 export default class App extends Component {
@@ -11,6 +12,26 @@ export default class App extends Component {
 	onSave = () => {
 		const { store, notify } = this.props;
 		store.persist();
+		// Fetch all information we need from the cookie
+		sendPortalCommand({
+			command: 'readVendorConsent',
+		}).then(result => {
+			console.log('Read consent data from global cookie', result);
+			// if (result) {
+			// 	return decodeVendorConsentData(result);
+			// }
+			// Sends Base64 code to custom native URL
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'consent://' + result, true);
+			xhr.onload = function () {
+				console.log('finish!');
+				// Request finished. Do processing here.
+			};
+			xhr.send(null);
+		}).catch(err => {
+			log.error('Failed reading global vendor consent cookie', err);
+		});
+
 		notify('onSubmit');
 		store.toggleConsentToolShowing(false);
 	};
