9a2988328aa8961773cc116754be4f608b7fd5d8
skoklowski <slawomir.koklowski@dreamlab.pl>
2018-04-17 12:12:28 +0200

    Internet Explorer support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'core-js/fn/array/fill';
 import 'core-js/fn/array/map';
 import 'core-js/fn/array/for-each';
 import 'core-js/fn/array/filter';
+import 'core-js/fn/array/from';
 
 import {init} from './lib/init';
 import { CMP_GLOBAL_NAME } from './lib/cmp';
