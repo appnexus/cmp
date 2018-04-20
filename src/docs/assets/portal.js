4ef377390c768ed85fe494de3a17834b6fb67165
Kris Hoyt <khoyt@appnexus.com>
2018-04-20 12:15:37 -0700

    Update CMP interface to match v1.1 spec

--- src/docs/assets/portal.js
+++ src/docs/assets/portal.js
@@ -8,7 +8,7 @@ const COOKIE_DOMAIN = parts.length > 1 ? `;domain=.${parts.slice(-2).join('.')}`
 const COOKIE_MAX_AGE = 33696000;
 const COOKIE_NAME = 'euconsent';
 
-const readVendorListPromise = fetch('./vendors.json', {
+const readVendorListPromise = fetch('./vendorlist.json', {
 	headers: {
 		'Accept': 'application/json',
 		'Content-Type': 'application/json'
