66a2ad41b7792a9c8533e20c77e3d3f89b569532
skoklowski <slawomir.koklowski@dreamlab.pl>
2018-05-09 13:27:31 +0200

    Polish translation

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -5,133 +5,68 @@
  * inline in each component.
  */
 export default {
-	en: {
+	pl: {
 		intro: {
-			title: '',
-			description: '',
-			acceptAll: '',
-			rejectAll: '',
-			showPurposes: ''
+			title: 'Szanowna Użytkowniczko, Szanowny Użytkowniku,',
+			description: `Z dniem 25 maja 2018 roku rozpoczyna obowiązywanie Rozporządzenie Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (określane popularnie jako „RODO”). 
+			RODO obowiązywać będzie w identycznym zakresie we wszystkich krajach Unii Europejskiej, a więc także w Polsce i wprowadza szereg zmian w zasadach regulujących przetwarzanie danych osobowych, które będą miały wpływ na wiele dziedzin życia, w tym na korzystanie z usług internetowych takich jak między innymi usługa portalu Onet i innych serwisów oferowanych przez Onet Holding sp. z o.o.
+			W tej informacji przedstawiamy skrót najważniejszych zagadnień dotyczących przetwarzania Twoich danych osobowych jakie może mieć miejsce po 25 maja 2018 roku w związku z korzystaniem z naszych usług. Prosimy o jej przeczytanie, nie zajmie to więcej niż kilka minut.
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Czym są dane osobowe</b><p>
+			Dane osobowe to zgodnie z RODO informacje o zidentyfikowanej lub możliwej do zidentyfikowania osobie fizycznej. W przypadku korzystania z naszych serwisów takimi danymi są np. adres e-mail, adres IP lub Twoje dane w koncie poczty elektronicznej czy koncie w innej usłudze Onetu. 
+			Dane osobowe mogą być zapisywane w plikach cookies lub podobnych technologiach (np. local storage) instalowanych przez nas lub naszych zaufanych partnerów na naszych stronach i urządzeniach których używasz podczas korzystania z naszych usług.
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Podstawa i cel przetwarzania</b><p>
+			Przetwarzanie danych osobowych wymaga podstawy prawnej. RODO przewiduje kilka rodzajów takich podstaw prawnych dla przetwarzania danych, a w przypadkach korzystania z naszych usług wystąpią co do zasady trzy z nich:
+			- niezbędność przetwarzania do zawarcia lub wykonania umowy, której jesteś stroną – umowa, o której mówią przepisy to w naszym przypadku regulamin danej usługi. Jeśli zatem zawieramy z Tobą umowę o realizację danej usługi – np. usługi, w ramach której zapewniamy Ci możliwość zapoznania z jednym z naszych serwisów w oparciu o treść regulaminu tego serwisu, to możemy przetwarzać Twoje dane w zakresie niezbędnym do realizacji tej umowy. W przypadku, gdy zakładasz u nas konto poczty elektronicznej, to umowa o dostarczenie tego konta upoważnia nas do przetwarzania danych niezbędnych do jego zapewnienia (np. danych podanych przez Ciebie w profilu tego konta). Bez tej możliwości nie bylibyśmy w stanie zapewnić Ci usługi, a Ty nie mógłbyś z niej korzystać.
+			- niezbędność przetwarzania do celów wynikających z prawnie uzasadnionych interesów realizowanych przez administratora lub przez stronę trzecią. Ta podstawa przetwarzania danych dotyczy przypadków, gdy ich przetwarzanie jest uzasadnione z uwagi na nasze usprawiedliwione potrzeby, co obejmuje między innymi konieczność zapewnienia bezpieczeństwa usługi (np. sprawdzenie, czy do Twojego konta nie loguje się nieuprawniona osoba), dokonanie pomiarów statystycznych, ulepszania naszych usług i dopasowania ich do potrzeb i wygody użytkowników (np. personalizowanie treści w usługach) jak również prowadzenie marketingu i promocji naszych własnych usług (np. jeśli interesujesz się historią i oglądasz materiały historyczne w portalu Onet lub na innych stronach internetowych, to możemy Ci wyświetlić reklamę dotyczącą artykułu w serwisie Onet Podróże o historycznych trasach krajoznawczych).
+			- Twoja dobrowolna zgoda. Ta podstawa przetwarzania danych będzie konieczna głównie w przypadku świadczenia Ci usług marketingowych przez podmioty trzecie (Zaufanych Partnerów, o których mowa poniżej) lub na ich rzecz. Chcąc przedstawić Ci reklamy, które będą dla Ciebie interesujące (np. przedstawić Ci reklamę produktu, którego możesz aktualnie potrzebować) reklamodawcy i ich przedstawiciele muszą mieć możliwość przetwarzania Twoich danych. Oczywiście udzielenie powyższej zgody jest całkowicie dobrowolne i jeśli nie chcesz, nie musisz jej udzielać. Udostępniane przez nas rozwiązanie umożliwia Ci także ograniczenie zakresu tej zgody, jak również jej zmianę w każdym momencie. Pozostałe przysługujące Ci w związku z udzieloną zgodą uprawnienia zostały opisane poniżej.
+			Twoje dane w ramach naszych usług przetwarzane będą wyłącznie w przypadku posiadania przez nas lub inny podmiot przetwarzający dane jednej z dopuszczonych przez RODO podstaw prawnych i wyłącznie w celu dostosowanym do danej podstawy, zgodnie z opisem powyżej. Twoje dane przetwarzane będą do czasu istnienia podstawy do ich przetwarzania – czyli w przypadku udzielenia zgody do momentu jej cofnięcia, ograniczenia lub innych działań z Twojej strony ograniczających tę zgodę, w przypadku niezbędności danych do wykonania umowy – przez czas jej wykonywania, a w przypadku, gdy podstawą przetwarzania danych jest uzasadniony interes administratora – do czasu istnienia tego uzasadnionego interesu.
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Administrator</b><p>
+			Administratorem Twoich danych osobowych będzie Onet – czyli Onet Holding sp. z o.o. z siedzibą w Warszawie (02 – 672), ul. Domaniewska 52, zarejestrowana w rejestrze przedsiębiorców Krajowego Rejestru Sądowego prowadzonym przez Sąd Rejonowy dla m. st. Warszawy Wydział XIII Gospodarczy Krajowego Rejestru Sądowego pod numerem KRS 0000420780, z kapitałem zakładowym w kwocie 95.000 zł (w całości wpłaconym), NIP: 5272677009, REGON: 146127300 – a także nasi Zaufani Partnerzy, czyli firmy i inne podmioty z którymi współpracujemy głównie w zakresie marketingowym. Listę Zaufanych Partnerów możesz sprawdzić w każdym momencie <a href="http://ocdn.eu/aops/mip/polityka/polityka-prywatnosci.html?test_kwrd=vappn" target="_blank">w tym miejscu</a> i zdecydować którym z nich chcesz wyrazić zgodę na przetwarzanie Twoich danych.
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Przekazywanie danych</b></p>
+			Twoje dane będzie przetwarzać Onet i Zaufani Partnerzy – jeśli wyrazisz im zgodę, ale mogą być one również powierzone do przetwarzania innym podmiotom. W każdym takim przypadku przekazanie danych nie uprawnia ich odbiorcy do dowolnego korzystania z danych, a jedynie do korzystania z nich w celach wyraźnie wskazanych przez Onet lub Zaufanego Partnera. Przekazywanie danych ma miejsce na ogół w przypadku współpracy z podwykonawcą (np. agencją marketingową) lub usługodawcą (np. dostawcą usług przechowywania danych). W każdym przypadku przekazanie danych nie zwalnia przekazującego z odpowiedzialności za ich przetwarzanie. Dane mogą być też przekazywane organom publicznym o ile upoważniają ich do tego obowiązujące przepisy i przedstawią odpowiednie żądanie. 
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Cookies</b><p>
+			Na naszych stronach internetowych i w aplikacjach używamy technologii, takich jak pliki cookie, local storage i podobnych służących do zbierania i przetwarzania danych osobowych oraz danych eksploatacyjnych w celu personalizowania udostępnianych treści i reklam oraz analizowania ruchu na naszych stronach. W ten sposób technologię tę wykorzystują również nasi Zaufani Partnerzy, którzy także chcą serwować reklamy jak najlepiej dopasowane do Twoich preferencji. Cookies to dane informatyczne zapisywane w plikach i przechowywane na Twoim urządzeniu końcowym (tj. twój komputer, tablet, smartphone itp.), które przeglądarka wysyła do serwera przy każdorazowym wejściu na stronę z tego urządzenia  podczas gdy odwiedzasz różne strony w Internecie. Szczegółową informację na temat plików cookie i ich funkcjonowania znajdziesz pod <a href="http://ofirmie.onet.pl/cookies" target="_blank">tym linkiem.</a>
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Twoje uprawnienia</b><p>
+			Zgodnie z RODO przysługują Ci następujące uprawnienia wobec Twoich danych i ich przetwarzania przez nas i Zaufanych Partnerów.
+			Jeśli udzieliłeś zgody na przetwarzanie danych możesz ją w każdej chwili wycofać. 
+			Masz również prawo żądania dostępu do Twoich danych osobowych, ich sprostowania, usunięcia, lub ograniczenia przetwarzania, prawo do przeniesienia danych, wyrażenia sprzeciwu wobec przetwarzania danych oraz prawo do wniesienia skargi do organu nadzorczego - GIODO. Uprawnienia powyższe przysługują także w przypadku prawidłowego przetwarzania danych przez administratora. <a href="http://ocdn.eu/aops/mip/polityka/polityka-prywatnosci.html?test_kwrd=vappn" target="_blank">W tym miejscu</a> znajdziesz dodatkowe informacje dotyczące przetwarzania danych i Twoich uprawnień.
+			<p style="margin-top: 10px; margin-bottom: 5px; line-spacing:single"><b>Zgoda<p>
+			Jeśli chcesz zgodzić się na przetwarzanie przez Zaufanych Partnerów Twoich danych osobowych zebranych w związku z korzystaniem przez Ciebie ze stron i aplikacji internetowych dostarczanych przez Onet Holding sp. z o.o. w celach marketingowych (obejmujących niezbędne działania analityczne i zestawianie w profile marketingowe na podstawie Twojej aktywności na stronach internetowych) oraz jeśli zgadzasz się na instalowanie plików cookies na Twoim urządzeniach i dostęp do tych plików przez Onet i Zaufanych Partnerów możesz w łatwy sposób wyrazić tą zgodę klikając w przycisk „Rozumiem” lub zamykając to okno. 
+			Jeśli nie chcesz wyrazić opisanej wyżej zgody lub ograniczyć jej zakres prosimy o kliknięcie w „Ustawienia zaawansowane”.  
+			W każdym momencie możesz także edytować Twoje preferencje w zakresie udzielonej  zgody w tym nawet wycofać ją całkowicie klikając w ustawienia zaawansowane lub przechodząc na naszą stronę polityki prywatności.</b>`,
+			acceptAll: 'Rozumiem',
+			showPurposes: 'Ustawienia zaawansowane'
 		},
 		details: {
-			title: '',
-			back: '',
-			save: ''
+			title: 'Ustawienia prywatności',
+			back: 'Cofnij',
+			save: 'Zapisz i zamknij'
 		},
 		purposes: {
-			active: '',
-			inactive: '',
-			showVendors: '',
-			cookies: {
-				menu: '',
-				title: '',
-				description: ''
-			},
-			purpose1: {
-				description: 'Allow storing or accessing information on a user’s device.'
-			},
-			purpose2: {
-				description: `Allow processing of a user’s data to provide and inform personalised advertising (including delivery, measurement, and reporting) based on a user’s preferences or interests known or inferred from data collected across multiple sites, apps, or devices; and/or accessing or storing information on devices  for that purpose.
-				Will include following Features:
-				<ul>
-					<li>Matching Data to Offline Sources - combining data from offline sources that were initially collected in other contexts.</li>
-					<li>Linking Devices - allow processing of a user’s data to connect such user across multiple devices.</li>
-					<li>Precise Geographic Location data - allow processing of a user’s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
-				</ul>`
+			active: 'Aktywna',
+			inactive: 'Nieaktywna',
+			showVendors: 'Pokaż listę naszych zaufanych partnerów',
+			purpose5: {
+				menu: 'Instalowanie plików cookies',
+				title: 'Instalowanie plików cookies itp. na Twoich urządzeniach i dostęp do tych plików.',
+				description: 'Na naszych stronach internetowych i w aplikacjach używamy technologii, takich jak pliki cookie, local storage i podobnych służących do zbierania i przetwarzania danych osobowych oraz danych eksploatacyjnych w celu personalizowania udostępnianych treści i reklam oraz analizowania ruchu na naszych stronach. Te pliki cookie pomagają poprawić jakość treści reklamowych na stronach i w aplikacjach. Dzięki tym technologiom możemy zapewnić Ci lepszą obsługę poprzez serwowanie reklam lepiej dopasowanych do Twoich preferencji i serwowanie treści, które mogą Cię bardziej zainteresować. Pliki te pozwalają nam również lepiej badać i analizować zainteresowanie naszymi stronami i treściami, które Ci serwujemy. Dzięki tym analizom możemy zadbać o jakość serwowanych przez nas treści.'
 			},
 			purpose3: {
-				description: `Allow processing of a user’s data to deliver content or advertisements and measure the delivery of such content or advertisements, extract insights and generate reports to understand service usage; and/or accessing or storing information on devices for that purpose.  
-				Will include following Features:
-				<ul>
-					<li>Matching Data to Offline Sources - combining data from offline sources that were initially collected in other contexts.</li>
-					<li>Linking Devices - allow processing of a user’s data to connect such user across multiple devices.</li>
-					<li>Precise Geographic Location data - allow processing of a user’s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
-				</ul>`
-			},
-			purpose4: {
-				description: `Allow processing of a user’s data to provide and inform personalised content (including delivery, measurement, and reporting) based on a user’s preferences or interests known or inferred from data collected across multiple sites, apps, or devices; and/or accessing or storing information on devices for that purpose.  
-				Will include following Features:
-				<ul>
-					<li>Matching Data to Offline Sources - combining data from offline sources that were initially collected in other contexts.</li>
-					<li>Linking Devices - allow processing of a user’s data to connect such user across multiple devices.</li>
-					<li>Precise Geographic Location data - allow processing of a user’s precise geographic location data in support of a purpose for which that certain third party has consent.</li>
-				</ul>`
-			},
-			customPurpose1: {
-				menu: '',
-				title: '',
-				description: ''
+				menu: 'Przetwarzanie danych osobowych',
+				title: 'Przetwarzanie danych osobowych w celach marketingowych przez Zaufanych Partnerów.',
+				description: 'Zgoda dotyczy przetwarzania Twoich danych osobowych przez Zaufanych Partnerów w celach marketingowych, które obejmują  także niezbędne działania analityczne i zestawianie w profile marketingowe na podstawie Twojej aktywności na stronach internetowych. Na podstawie tej zgody, Twoje dane, w tym zebrane informacje o Twojej aktywności na stronach internetowych, będą wykorzystywane do prezentowania Ci reklam produktów i usług naszych Partnerów biznesowych, dopasowanych do Twoich zainteresowań.'
 			}
 		},
 		vendors: {
-			title: '',
-			rejectAll: '',
-			acceptAll: '',
-			company: '',
-			offOn: '',
-			description: '',
-			moreChoices: ''
+			title: 'Nasi zaufani partnerzy',
+			company: 'Partner',
+			offOn: 'Akceptuję',
+			description: 'Poniżej lista podmiotów, którym możesz wyrazić zgodę opisaną w poprzednim oknie. Dzięki tej zgodzie możliwe będzie lepsze dopasowania treści i reklam prezentowanych przez Zaufanych Partnerów do Twoich zainteresowań. Jeśli nie chcesz wyrazić zgody danemu podmiotowi odznacz wybraną opcję.',
+			moreChoices: 'Edytuj zgody dla naszych Zaufanych Partnerów'
 		},
 		footer: {
-			message: '',
-			consentLink: ''
-		}
-	},
-	de: {
-		intro: {
-			title: 'Diese Website verwendet Cookies',
-			description: 'Wir und unsere Partner verwenden sogenannte Cookies (kleine Textdateien) im Webbrowser um zu verstehen, was unsere Besucher interessiert und entsprechend relevante Inhalte und Werbung anbieten zu können. Zukünftig benötigen wir wahrscheinlich ihr/euer Einverständnis dazu. Ein Beispiel, wie dies aussehen könnte, finden sie/findet ihr unter dieser Erklärung ',
-			acceptAll: 'Alle Cookies akzeptieren',
-			rejectAll: 'Alle Cookies ablehnen',
-			showPurposes: 'Verwendungszwecke zeigen'
-		},
-		details: {
-			title: 'Datenschutzeinstellungen',
-			back: 'Abbrechen',
-			save: 'Sichern und Beenden'
-		},
-		purposes: {
-			active: 'Aktiv',
-			inactive: 'Inaktiv',
-			showVendors: '',
-			cookies: {
-				menu: 'Wie wir Cookies einsetzen',
-				title: 'Diese Website verwendet Cookies',
-				description: 'Unsere Partner und wir setzen Cookies (kleine Textdateien) und sammeln Informationen während des Surfens im Web in diesem Browser. Dies dient dazu zu verstehen, was unsere Besucher interessiert und entsprechend relevante Inhalte und Werbung anbieten zu können.'
-			},
-			purpose1: {
-				menu: 'Zugriff auf ein Gerät',
-				title: 'Zugriff auf ein Gerät',
-				description: 'Die Erlaubnis zum Speichern und Abrufen von Informationen auf dem Gerät eines Website-Besuchers.Das ist notwendig, um Cookies im Web-Browser zu speichern und zur Anzeige relevanter Informationen und Werbung abrufen zu können.'
-			},
-			purpose2: {
-				menu: 'Persönlich angepaßte Werbung',
-				title: 'Persönlich angepaßte Werbung',
-				description: 'Die Erlaubnis, Besucherdaten so zu verarbeiten und/oder zu speichern und abzurufen, dass persönlich angepaßte Werbung angeboten und angezeigt werden kann (dies umfaßt die Auslieferung, Messung und die Erstellung von Berichten darüber). Dies erfolgt auf der Basis bekannter Präferenzen oder Interessen, oder durch das Schließen auf Präferenzen oder Interessen durch die Erfassung von Daten auch über verschiedene Websites, Apps oder Geräte hinweg zu diesem Zweck.'
-			},
-			purpose3: {
-				menu: 'Analysen',
-				title: 'Analysen',
-				description: 'Die Erlaubnis, Besucherdaten zur Anzeige von Inhalten oder Werbung zu verarbeiten, und zur Messung der Auslieferung solcher Inhalte oder Werbung. Umfasst ist die Gewinnung von Erkenntnissen und die Generierung von Berichten um die Nutzung des angebotenen Service zu verstehen, und/oder das Abrufen oder Speichern von Informationen auf Geräten zu diesem Zweck.'
-			},
-			purpose4: {
-				menu: 'Persönlich angepasste Inhalte',
-				title: 'Persönlich angepasste Inhalte',
-				description: 'Die Erlaubnis, Besucherdaten zur Anzeige von personalisierten Inhalten zu verarbeiten, und zur Messung der Auslieferung. Umfasst ist die Gewinnung von Erkenntnissen darüber und die Generierung von Berichten dazu. Dies erfolgt auf der Basis bekannter Präferenzen oder Interessen, oder durch das Schließen auf Präferenzen oder Interessen durch die Erfassung von Daten auch über verschiedene Websites, Apps oder Geräte hinweg zu diesem Zweck.'
-			}
-		},
-		vendors: {
-			title: 'Unsere Partner',
-			rejectAll: 'Alle ablehnen',
-			acceptAll: 'Alle akzeptieren',
-			company: 'Unternehmen',
-			offOn: 'Aus/An',
-			description: 'Helfen Sie uns, Ihnen einen besseren Service zu bieten! Unsere Partner verwenden Cookies Ihres Browsers, um quer durch das Web zu verstehen, was Sie interessiert und Ihnen entsprechend relevante Inhalte und Werbung anzubieten.',
-			moreChoices: 'Weitere Auswahlmöglichkeiten'
-		},
-		footer: {
-			message: 'Du kannst deine Datenschutz-Einstellungen bearbeiten',
-			consentLink: 'hier'
+			message: 'Zmień swoje ustawienia prywatności klikając',
+			consentLink: 'tutaj'
 		}
 	}
 };
