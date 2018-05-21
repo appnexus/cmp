57187582fc46896627b9d780122d731c26a54d5e
mdurrant <mdurrant@expressmedia.com>
2018-05-20 18:51:45 -0600

    banner mobile responsiveness (rc)

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,7 +1,6 @@
 import { h, Component } from 'preact';
 import style from './banner.less';
 import Label from '../label/label';
-import Panel from '../panel/panel';
 import ChevronIcon from '../chevronicon/chevronicon';
 
 class LocalLabel extends Label {
@@ -12,8 +11,6 @@ class LocalLabel extends Label {
 
 const PANEL_COLLECTED = 0;
 const PANEL_PURPOSE = 1;
-
-const BANNER_OFFSET = 20;
 
 export default class Banner extends Component {
 
@@ -43,37 +40,29 @@ export default class Banner extends Component {
 		this.props.onShowModal(true);
 	};
 
-	calculateBannerHeight = () => {
-		const {isExpanded} = this.state;
-		const {isShowing} = this.props;
-		const {bannerRef, messageRef} = this;
-		if (bannerRef) {
-			const bannerHeight = bannerRef.getBoundingClientRect().height;
-			const messageHeight = messageRef.getBoundingClientRect().height;
-			let bannerBottom = 0;
-			if (!isExpanded && isShowing) {
-				bannerBottom = messageHeight - bannerHeight;
-			}
-			else if (!isShowing) {
-				bannerBottom = -bannerHeight - BANNER_OFFSET;
-			}
+	// Ensures the right aligned buttons are centered vertically but won't slide around when options are expanded.
+	calculateButtonOffset = () => {
+		const {messageRef} = this;
 
-			if (bannerBottom !== this.state.bannerBottom) {
-				this.setState({bannerBottom});
+		if (messageRef) {
+			const height = messageRef.getBoundingClientRect().height;
+			const buttonOffset = (height - 48) / 2;
+
+			if (!this.state.buttonOffset) {
+				this.setState({buttonOffset});
 			}
 		}
 	};
 
 	render(props, state) {
 		const {isShowing, onSave} = props;
-		const {selectedPanelIndex, bannerBottom, isExpanded} = state;
-		this.calculateBannerHeight();
+		const {selectedPanelIndex, buttonOffset, isExpanded} = state;
+		this.calculateButtonOffset();
 
 		return (
 			<div
 				ref={el => this.bannerRef = el}
 				class={style.banner}
-				style={{bottom: `${bannerBottom}px`}}
 			>
 				<div class={style.content}>
 					<div
@@ -85,52 +74,61 @@ export default class Banner extends Component {
 							Ads help us run this site
 							</div>
 							When you visit our site, pre-selected companies may access and use certain information on your device to serve relevant ads or personalized content.
-							<a class={style.learnMoreMobile} onClick={this.handleLearnMore}>Learn More</a>
 
 							<div class={style.options}>
-								<div class={style.option}>
+								<div class={[style.option, selectedPanelIndex === PANEL_COLLECTED && isExpanded ? style.expanded : ''].join(' ')}>
 									<a onClick={this.handleInfo(PANEL_COLLECTED)}>
-										<ChevronIcon class={[style.expand, selectedPanelIndex === PANEL_COLLECTED && isExpanded ? style.expanded : ''].join(' ')}  />
+										<ChevronIcon />
 										Information that may be used.
 									</a>
+
+									<div class={style.optionDetails}>
+										<ul>
+											<li>Type of browser and its settings</li>
+											<li>Information about the device's operating system</li>
+											<li>Cookie information</li>
+											<li>Information about other identifiers assigned to the device</li>
+											<li>The IP address from which the device accesses a client's website or mobile application</li>
+											<li>Information about the user's activity on that device, including web pages and mobile apps visited or used</li>
+											<li>Information about the geographic location of the device when it accesses a website or mobile application</li>
+										</ul>
+									</div>
 								</div>
-								<div class={style.option}>
+								<div class={[style.option, selectedPanelIndex === PANEL_PURPOSE && isExpanded ? style.expanded : ''].join(' ')}>
 									<a onClick={this.handleInfo(PANEL_PURPOSE)}>
-										<ChevronIcon class={[style.expand, selectedPanelIndex === PANEL_PURPOSE && isExpanded ? style.expanded : ''].join(' ')}  />
+										<ChevronIcon />
 										Purposes for storing information.
 									</a>
+
+									<div class={style.optionDetails}>
+										<ul>
+											<li>Storage and access of information</li>
+											<li>Ad selection and delivery</li>
+											<li>Content selection and delivery</li>
+											<li>Personalization</li>
+											<li>Measurement</li>
+										</ul>
+									</div>
 								</div>
 							</div>
 						</div>
 						<div class={style.consent}>
-							<a class={style.learnMore} onClick={this.handleLearnMore}>Learn More</a>
-							<a class={style.continue} onClick={onSave}>Continue to site</a>
+							<a
+								class={style.learnMore}
+								onClick={this.handleLearnMore}
+								style={{top: `${buttonOffset}px`}}
+							>
+								Learn More
+							</a>
+							<a
+								class={style.continue}
+								onClick={onSave}
+								style={{top: `${buttonOffset}px`}}
+							>
+								Continue to site
+							</a>
 						</div>
 					</div>
-					<Panel
-						selectedIndex={selectedPanelIndex}
-						class={style.infoExpanded}>
-						<div class={style.infoExpanded}>
-							<ul>
-								<li>Type of browser and its settings</li>
-								<li>Information about the device's operating system</li>
-								<li>Cookie information</li>
-								<li>Information about other identifiers assigned to the device</li>
-								<li>The IP address from which the device accesses a client's website or mobile application</li>
-								<li>Information about the user's activity on that device, including web pages and mobile apps visited or used</li>
-								<li>Information about the geographic location of the device when it accesses a website or mobile application</li>
-							</ul>
-						</div>
-						<div class={style.infoExpanded}>
-							<ul>
-								<li>Storage and access of information</li>
-								<li>Ad selection and delivery</li>
-								<li>Content selection and delivery</li>
-								<li>Personalization</li>
-								<li>Measurement</li>
-							</ul>
-						</div>
-					</Panel>
 				</div>
 			</div>
 		);
