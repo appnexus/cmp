e12a125a7780e878a0d1632d11ed6683169fbecc
Artur Szydlo <aszydlo@dreamlab.pl>
2018-05-24 10:54:24 +0200

    chack statment before import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import 'core-js/fn/array/reduce';
-import 'core-js/fn/array/fill';
-import 'core-js/fn/array/map';
-import 'core-js/fn/array/for-each';
-import 'core-js/fn/array/filter';
-import 'core-js/fn/set';
+!Array.reduce && import ('core-js/fn/array/reduce');
+!Array.fill && import ('core-js/fn/array/fill');
+!Array.map && import ('core-js/fn/array/map');
+!Array.forEach && import ('core-js/fn/array/for-each');
+!Array.filter && import ('core-js/fn/array/filter');
 !Array.from && import ('core-js/fn/array/from');
-
+import 'core-js/fn/set';
 import {init} from './lib/init';
 import { CMP_GLOBAL_NAME } from './lib/cmp';
 
