0f35ff954df0a6d1c108c7ac4b62e0d59609b687
Artur Szydlo <aszydlo@dreamlab.pl>
2018-05-24 10:37:26 +0200

    don't import Array.from when already exist

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import 'core-js/fn/array/fill';
 import 'core-js/fn/array/map';
 import 'core-js/fn/array/for-each';
 import 'core-js/fn/array/filter';
-import 'core-js/fn/array/from';
 import 'core-js/fn/set';
+!Array.from && import ('core-js/fn/array/from');
 
 import {init} from './lib/init';
 import { CMP_GLOBAL_NAME } from './lib/cmp';
