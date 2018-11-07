575d4418039475fb74bb4578319c1b358b2ac343
Amon-Ra <amon.raj@gmail.com>
2018-11-07 16:21:07 +0100

    Remove cookies on init

--- src/custom.js
+++ src/custom.js
@@ -1,6 +1,71 @@
+
 import log from './lib/log';
 import { CMP_GLOBAL_NAME, CMP_CALL_NAME, CMP_LOCATOR_NAME } from "./lib/cmp";
 
+
+//datediff is an integer in days
+const createCookie = (key, value, datediff) => {
+    let date = new Date();
+    // console.log(value)
+    // console.log(datediff)
+    date.setDate(date.getDate() + parseInt(datediff));
+    let expiration = date.toUTCString();
+    let cookie = escape(key) + "=" + escape(value) + ";expires=" + expiration + ";";
+    document.cookie = cookie;
+}
+
+const readCookie = name => {
+    var key = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(key) === 0) {
+            return cookie.substring(key.length, cookie.length);
+        }
+    }
+    return null;
+}
+// window.alert("INIT");
+// console.log(readCookie('euconsent'));
+function removeCookies(){
+    if(!readCookie('euconsent')){
+    // (function () {
+        var cookies = document.cookie.split("; ");
+        for (var c = 0; c < cookies.length; c++) {
+            var d = window.location.hostname.split(".");
+            // console.log(d)
+            // console.log(cookies[c]);
+            while (d.length > 0) {
+                let domain = d.join('.');
+                // console.log(domain);
+                let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-2017 00:00:01 GMT; domain=' + domain + ' ;path=';
+                let p = location.pathname.split('/');
+                // console.log(cookieBase);
+                document.cookie = cookieBase + '/';
+                while (p.length > 0) {
+                    document.cookie = cookieBase + p.join('/');
+                    p.pop();
+                };
+                d.shift();
+                cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-2017 00:00:01 GMT;path=';
+                p = location.pathname.split('/');
+                // console.log(cookieBase);
+                document.cookie = cookieBase + '/';
+                while (p.length > 0) {
+                    document.cookie = cookieBase + p.join('/');
+                    p.pop();
+                };                           
+            }
+        }
+    // })();     
+    // window.alert("INIT")
+    }
+}
+
+removeCookies();
 
 function handleCustomVendors(cmp,vendorList, vendorConsents) {
     const vendors = vendorList['vendors']
@@ -10,12 +75,12 @@ function handleCustomVendors(cmp,vendorList, vendorConsents) {
         if (vendorConsents[vendorId] === false){
             const vendor = vendors[vendorId]
             if(vendor)
-               block(vendor)                            
+               window.yett.block(vendor)                            
         }
         if (vendorConsents[vendorId] === true){
             const vendor = vendors[vendorId]
             if(vendor)
-               unblock(vendor)
+               window.yett.unblock(vendor)
         }                
     })   
     if(cmp.reload) window.location.reload() 
@@ -29,10 +94,10 @@ function handleConsentResult(cmp,vendorList, vendorConsents) {
 		log.debug('No consent data found. Showing consent tool');
 		cmp('showConsentTool');
 	}
-	else if (!listVersion) {
+	else if (!listVersion || !vendorListVersion) {
 		log.debug('Could not determine vendor list version. Not showing consent tool');
 	}
-	else if (vendorListVersion !== listVersion) {
+	else if ( vendorListVersion !== listVersion) {
 		log.debug(`Consent found for version ${vendorListVersion}, but received vendor list version ${listVersion}. Showing consent tool`);
 		cmp('showConsentTool');
 	}
@@ -114,18 +179,20 @@ function checkConsent(cmp) {
                 });
             }
         };
+        let cmp_config = window.CMP_CONFIG || [];
         cmp.config = {
-            globalVendorListLocation: '/docs/vendorlist.json',
-            customPurposeListLocation: '/docs/purposes.json',
-            globalConsentLocation: '/docs/portal.html',
+            globalVendorListLocation: 'http://oondeo.es/gdpr/vendorlist.json',
+            // customPurposeListLocation: '/docs/purposes.json',
+            // globalConsentLocation: '/docs/portal.html',
             storeConsentGlobally: false,
             storePublisherData: false,
             // gdprApplies: true,
             // localization: {},
             // forceLocale: 'en-us'					
-            logging: 'debug',
+            // logging: 'debug',
             bannerModal: true,
-            bannerLogo: ''                        
+            bannerLogo: '',
+            ...cmp_config                       
         }
         cmp.reload = false;
         cmp('addEventListener', 'onSubmit', function() {
@@ -140,15 +207,44 @@ function checkConsent(cmp) {
 // }    
     
 // })(window, document);
-// let YETT_BLACKLIST = window.YETT_BLACKLIST || []; 
-// let YETT_WHITELIST = window.YETT_WHITELIST || [];
+
+import 'core-js/fn/array/reduce';
+import 'core-js/fn/array/fill';
+import 'core-js/fn/array/map';
+import 'core-js/fn/array/for-each';
+import 'core-js/fn/array/filter';
+import 'core-js/fn/array/from';
+import 'core-js/fn/set';
+
+import {init} from './lib/init';
+
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(function(){ removeCookies(); }, 500);
+    // in development, set up HMR:
+        if (module.hot) {
+            console.log("Development Environment");
+            require('preact/devtools');   // turn this on if you want to enable React DevTools!
+            module.hot.accept('./components/app', () => requestAnimationFrame(init) );
+        }
+        const {config} = window[CMP_GLOBAL_NAME] || {};
+        init(config);     
+    });
+
+
+let YETT_BLACKLIST = window.YETT_BLACKLIST || []; 
+let YETT_WHITELIST = window.YETT_WHITELIST || [];
 window.YETT_BLACKLIST = [
     'iframe:src/.*/sandbox:',
-    'script:src//'
+    'script:src//',
+    'script:src/.*/',
+    ...YETT_BLACKLIST
 ];
 window.YETT_WHITELIST = [
     /cmp\.bundle\.js/,
-    /cmp\.complete\.bundle\.js/
+    /cmp\.custom\.bundle\.js/,
+    /cmp\.complete\.bundle\.js/,
+    ...YETT_WHITELIST
 ];
 
-import {block,unblock} from 'yett';
+// import {block,unblock} from 'yett';
+// console.log(block);
--- src/docs/assets/inline.js
+++ src/docs/assets/inline.js
@@ -1,4 +1,5 @@
 (function(){
+    window.alert("inline")
     var div = document.createElement('div')
     div.innerHTML = '<a href="https://github.com/snipsco/yett/blob/master/docs/inline.js" target="_blank">The inlined &lt;script&gt; tag has been executed.</a>'
     document.getElementById('content').appendChild(div)
