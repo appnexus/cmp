16daf0c3c4f11791f92efb1047fdd02775aae8ae
Guillaume Riou <guiloua2@hotmail.com>
2019-02-19 16:19:58 -0500

    Adds our cmp id

--- src/lib/init.js
+++ src/lib/init.js
@@ -11,7 +11,8 @@ import config from './config';
 const CMP_VERSION = 1;
 
 // CMP_ID is the ID of your consent management provider according to the IAB. Get an ID here: https://advertisingconsent.eu/cmps/
-const CMP_ID = 0;
+// 75 is m32.media's cmp id.
+const CMP_ID = 75;
 
 // The cookie specification version, as determined by the IAB. Current is 1.
 const COOKIE_VERSION = 1;
