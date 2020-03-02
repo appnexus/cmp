1792a801bd81d0ace7fdf5625153898e1f1b0255
Maciej Ząbek <mzabek2@mzabek2-nm.grupa.onet>
2020-03-02 11:38:26 +0100

    reverted translations

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -9,50 +9,50 @@ export default {
 		intro: {
 			title: 'Szanowna Użytkowniczko, Szanowny Użytkowniku,',
 			description: `<p> Zanim klikniesz „Przejdź do serwisu” lub zamkniesz to okno, prosimy o przeczytanie tej informacji. Prosimy w niej o Twoją dobrowolną zgodę na przetwarzanie Twoich danych osobowych przez naszych partnerów biznesowych oraz przekazujemy informacje o tzw. cookies i o przetwarzaniu przez nas Twoich danych osobowych. <span style="font-weight: bold">Klikając „Przejdź do serwisu” lub zamykając okno przez kliknięcie w znaczek X, zgadzasz się na poniższe.</span> Możesz też odmówić zgody lub ograniczyć jej zakres.</p>
-
+			
     <p style="margin: 25px 0 10px 0; font-weight: 900">Zgoda</p>
-
+	
     <p> Jeśli chcesz zgodzić się na przetwarzanie przez <a onclick="__cmp('showVendors')">Zaufanych Partnerów</a> Grupy RAS Polska Twoich danych osobowych, które udostępniasz w historii przeglądania stron i aplikacji internetowych oraz danych lokalizacyjnych generowanych przez Twoje urządzenie, w celach marketingowych (obejmujących zautomatyzowaną analizę Twojej aktywności na stronach internetowych i w aplikacjach w celu ustalenia Twoich potencjalnych zainteresowań dla dostosowania reklamy i oferty) w tym na umieszczanie znaczników internetowych (cookies itp.) na Twoich urządzeniach i odczytywanie takich znaczników, kliknij przycisk „Przejdź do serwisu” lub zamknij to okno. </p>
-
+	
 <p>Jeśli nie chcesz wyrazić zgody lub ograniczyć jej zakres, kliknij „Ustawienia zaawansowane”. </p>
 
 <p>Wyrażenie zgody jest dobrowolne. Możesz edytować zakres zgody, w tym wycofać ją całkowicie, klikając „Ustawienia zaawansowane” lub przechodząc na naszą stronę
 <a href="https://polityka-prywatnosci.onet.pl/index.html" target="_blank" rel="noopener">polityki prywatności</a>.</p>
-
+            
            <p>Powyższa zgoda dotyczy przetwarzania Twoich danych osobowych w celach marketingowych Zaufanych Partnerów. <a onclick="__cmp('showVendors')">Zaufani Partnerzy</a> to firmy z obszaru e-commerce i reklamodawcy oraz działające w ich imieniu domy mediowe i podobne organizacje, z którymi Grupa RAS Polska współpracuje lub które polecane są przez międzynarodowe organizacje branżowe takie jak np. organizacja IAB (Interactive Advertising Bureau). Niektórzy z <a onclick="__cmp('showVendors')">Zaufanych Partnerów</a> mogą przetwarzać Twoje dane w węższym zakresie,  niektórzy z nich mogą też przetwarzać te dane w oparciu o inną podstawę niż Twoja zgoda (np. w oparciu o uzasadniony interes administratora). Kompletną listę Zaufanych Partnerów wraz z zakresem danych przetwarzanych przez nich w oparciu o Twoją zgodę lub inną podstawę prawną znajdziesz bezpośrednio pod <a onclick="__cmp('showVendors')">tym linkiem</a>.</p>
-
+		   
 <p>Podmioty z Grupy RAS Polska w ramach udostępnianych przez siebie usług internetowych przetwarzają Twoje dane opisane na wstępie, a także zagregowane informacje o wieku, płci i lokalizacji (wielkości miasta z którego pochodzisz itp.) pochodzące z Twoich kont w usługach świadczonych przez podmioty z Grupy RAS Polska w celach marketingowych w oparciu o prawnie uzasadniony, wspólny interes podmiotów Grupy RAS Polska. Dane te są jednocześnie danymi niezbędnymi do realizacji przez nas umowy zawartej z Tobą, która dotyczy korzystania przez Ciebie z naszych usług i serwisów internetowych, ze względu na właściwości tych usług. Treść tej umowy określa regulamin portalu Onet - <a href="https://polityka-prywatnosci.onet.pl/regulamin.html" target="_blank" rel="noopener">https://polityka-prywatnosci.onet.pl/regulamin.html</a> - lub regulaminy innych usług świadczonych przez podmioty z Grupy RAS Polska.</p>
 
 <p>Przetwarzanie takie nie wymaga dodatkowej zgody z Twojej strony, ale możesz mu się w każdej chwili sprzeciwić. O ile nie zdecydujesz inaczej, dokonując stosownych zmian ustawień w Twojej przeglądarce, podmioty z Grupy RAS Polska będą również instalować na Twoich urządzeniach pliki cookies i podobne oraz odczytywać informacje z takich plików. Bliższe informacje o cookies znajdziesz w akapicie „Cookies” pod koniec tej informacji.
 </p>
     <p style="margin: 25px 0 10px 0; font-weight: 900">Administrator danych osobowych</p>
     <p> Administratorem Twoich danych osobowych jest podmiot z Grupy RAS Polska, z którego serwisu lub aplikacji korzystasz. Naszą nazwę znajdziesz zawsze na samym dole strony po prawej stronie, a logo na planszy zawierającej niniejszą informację. Informację o powołaniu przez nas inspektora ochrony danych i jego danych kontaktowych znajdziesz też na samym dole strony w polityce prywatności. Administratorami Twoich danych osobowych będą także pozostałe podmioty z Grupy RAS Polska, których pełne dane możesz sprawdzić <a href="https://polityka-prywatnosci.onet.pl/grupa-rasp.html" target="_blank" rel="noopener">pod tym linkiem</a>. Jeżeli wyrazisz zgodę, o którą wyżej prosimy, administratorami Twoich danych osobowych będą także nasi <a onclick="__cmp('showVendors')">Zaufani Partnerzy</a>. Listę Zaufanych Partnerów możesz sprawdzić w każdym momencie na stronie naszej <a href="https://polityka-prywatnosci.onet.pl/index.html" target="_blank" rel="noopener">polityki prywatności</a> i tam też zmodyfikować lub cofnąć swoje zgody.</p>
-
+        
 		<p style="margin: 25px 0 10px 0; font-weight: 900">Podstawa i cel przetwarzania</p>
-
+       
 	   <p > Twoje dane przetwarzamy w następujących celach:</p>
         <br>
         <ul style="list-style-position: inside; list-style-type: disc;">
-
+		
             <li style="margin-bottom: 20px">1. Jeśli zawieramy z Tobą umowę o realizację danej usługi (np. usługi zapewniającej Ci możliwość zapoznania się z jednym z naszych serwisów w oparciu o treść regulaminu tego serwisu), to możemy przetwarzać Twoje dane w zakresie niezbędnym do realizacji tej umowy. Gdy zakładasz konto poczty elektronicznej, to umowa o dostarczenie tego konta upoważnia nas do przetwarzania danych niezbędnych do jego zapewnienia (np. danych podanych przez Ciebie w profilu tego konta). Bez tego nie bylibyśmy w stanie zapewnić Ci usługi, a Ty nie mógłbyś z niej korzystać. Niepodanie takich danych uniemożliwi nam zawarcie i wykonanie umowy.</li>
-
+			
             <li style="margin-bottom: 20px">2. Zapewnianie bezpieczeństwa usługi (np. sprawdzenie, czy do Twojego konta nie loguje się nieuprawniona osoba), dokonanie pomiarów statystycznych, ulepszanie naszych usług i dopasowanie ich do potrzeb i wygody użytkowników (np. personalizowanie treści w usługach), jak również prowadzenie marketingu <span style="font-weight: bold">bezpośredniego</span> (także na zlecenie reklamodawców) lub promocji własnych usług (np. jeśli interesujesz się historią i oglądasz materiały historyczne w portalu Onet lub na innych stronach internetowych, to możemy Ci wyświetlić reklamę dotyczącą artykułu w serwisie Onet Podróże o historycznych trasach krajoznawczych). Takie przetwarzanie danych to realizacja naszych prawnie uzasadnionych interesów.</li>
-
+			
             <li>3. Za Twoją zgodą usługi marketingowe dostarczą Ci nasi <a onclick="__cmp('showVendors')">Zaufani Partnerzy</a> oraz my dla podmiotów trzecich. Aby móc pokazać interesujące Cię reklamy (np. produktu, którego możesz potrzebować) reklamodawcy i ich przedstawiciele chcieliby mieć możliwość przetwarzania Twoich danych związanych z odwiedzanymi przez Ciebie stronami internetowymi. Udzielenie takiej zgody jest dobrowolne, nie musisz jej udzielać, nie pozbawi Cię to dostępu do naszych usług. Masz również możliwość ograniczenia zakresu lub zmiany zgody w dowolnym momencie.</li>
         </ul>
-
+        
 		<br>
         <p> Twoje dane przetwarzane będą do czasu istnienia podstawy do ich przetwarzania, czyli w przypadku udzielenia zgody do momentu jej cofnięcia, ograniczenia lub innych działań z Twojej strony ograniczających tę zgodę, w przypadku niezbędności danych do wykonania umowy, przez czas jej wykonywania i ewentualnie okres przedawnienia roszczeń z niej (zwykle nie więcej niż 3 lata, a maksymalnie 6 lat), a w przypadku, gdy podstawą przetwarzania danych jest uzasadniony interes administratora, do czasu zgłoszenia przez Ciebie skutecznego sprzeciwu.</p>
-
+      
 	  <p style="margin: 25px 0 10px 0; font-weight: 900">Przekazywanie danych</p>
-
+		
         <p> Administratorzy danych mogą powierzać Twoje dane podwykonawcom IT, księgowym, agencjom marketingowym etc. Zrobią to jedynie w niezbędnym zakresie i na podstawie umowy o powierzenie przetwarzania danych zobowiązującej taki podmiot do odpowiedniego zabezpieczenia danych i niekorzystania z nich do własnych celów.</p>
-
+		
         <p style="margin: 25px 0 10px 0; font-weight: 900">Cookies</p>
-
+		
         <p> Na naszych stronach i w aplikacjach używamy znaczników internetowych takich jak pliki np. cookie lub local storage do zbierania i przetwarzania danych osobowych w celu personalizowania treści i reklam oraz analizowania ruchu na stronach, aplikacjach i w Internecie. W ten sposób technologię tę wykorzystują również podmioty z Grupy RAS Polska oraz nasi <a onclick="__cmp('showVendors')">Zaufani Partnerzy</a>, którzy także chcą dopasowywać reklamy do Twoich preferencji. Cookies to dane informatyczne zapisywane w plikach i przechowywane na Twoim urządzeniu końcowym (tj. twój komputer, tablet, smartphone itp.), które przeglądarka wysyła do serwera przy każdorazowym wejściu na stronę z tego urządzenia, podczas gdy odwiedzasz strony w Internecie. Szczegółową informację na temat plików cookie i ich funkcjonowania znajdziesz <a href="https://polityka-prywatnosci.onet.pl/grupa-rasp.html" target="_blank" rel="noopener">pod tym linkiem</a>. Pod tym linkiem znajdziesz także informację o tym jak zmienić ustawienia przeglądarki, aby ograniczyć lub wyłączyć funkcjonowanie plików cookies itp. oraz jak usunąć takie pliki z Twojego urządzenia.</p>
-
-
+		
+             
 		<p style="margin: 25px 0 10px 0; font-weight: 900">Twoje uprawnienia</p>
         <p> Przysługują Ci następujące uprawnienia wobec Twoich danych i ich przetwarzania przez nas, inne podmioty z Grupy RAS Polska i <a onclick="__cmp('showVendors')">Zaufanych Partnerów</a>:</p>
 		<br>
