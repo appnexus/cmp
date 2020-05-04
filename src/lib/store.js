1ee8d0781242a1969e4e84a7f02ae242fbea9765
Maciej Ząbek <maciej.jan.zabek@gmail.com>
2020-05-04 13:55:51 +0200

    checking persisted version since v1 string is valid after libs update

--- src/lib/store.js
+++ src/lib/store.js
@@ -31,8 +31,9 @@ export default class Store {
 
 		// decoding to check if string is compatible
 		const decodedConsentString = decodeConsentData(consentString);
-		this.persistedConsentString = decodedConsentString ? consentString : '';
-		this.persistedConsentData = decodedConsentString || {};
+		const isTCFv2Compatible = decodedConsentString && decodedConsentString.version > 1;
+		this.persistedConsentString = isTCFv2Compatible ? consentString : '';
+		this.persistedConsentData = isTCFv2Compatible ? decodedConsentString : {};
 
 		this.tcModel = Object.assign(
 			tcModel,
