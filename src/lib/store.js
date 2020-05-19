1ef3ca0494a8a07d0c2358567a9dc282ef0aaf00
aszydlo <artur.szydlo@dreamlab.pl>
2020-05-19 11:13:15 +0200

    revert arg name

--- src/lib/store.js
+++ src/lib/store.js
@@ -120,8 +120,8 @@ export default class Store {
 			}
 		}
 
-		const unsetMissingVendors = vendors => {
-			vendors.forEach((value, id) => availableIds.has(id) || vendors.unset(id));
+		const unsetMissingVendors = consents => {
+			consents.forEach((value, id) => availableIds.has(id) || consents.unset(id));
 		};
 
 		unsetMissingVendors(vendorConsents);
