97318be82e39a00bf66c2e1bdabe55763f1127e8
aszydlo <artur.szydlo@dreamlab.pl>
2021-02-09 08:22:40 +0100

    formatting

--- src/lib/macros.js
+++ src/lib/macros.js
@@ -13,23 +13,12 @@ const macros = {
 	// RASCH translation
 	'de_rasch': {
 		'_PUBLISHER_': 'Ringier Axel Springer Schweiz',
-		'_POLICY_': prepareMacro(`<p>Wenn Sie der Datenbearbeitung basierend auf berechtigtem Interesse widersprechen oder ihre Einwilligung widerrufen
-	wollen, können Sie dies in den entsprechenden Datenschutzbestimmungen in Kapitel 4.3 tun:</p>
-<p><a href="https://www.beobachter.ch/service/beobachter-und-guider-datenschutzbestimmungen" target="_blank"
-	  rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.beobachter.ch/service/beobachter-impressum"
-														target="_blank" rel="noopener">Impressum</a> von Beobachter</p>
-<p><a href="https://www.handelszeitung.ch/datenschutz" target="_blank" rel="noopener">Datenschutzbestimmungen</a> und <a
-	href="https://www.handelszeitung.ch/impressum" target="_blank" rel="noopener">Impressum</a> von HZ, Bilanz,
-	Schweizer Versicherung</p>
-<p><a href="https://www.schweizer-illustrierte.ch/datenschutzbestimmungen-lang" target="_blank" rel="noopener">Datenschutzbestimmungen</a>
-	und <a href="https://www.schweizer-illustrierte.ch/impressum" target="_blank" rel="noopener">Impressum</a> der
-	Schweizer Illustrierten</p>
-<p><a href="https://www.cash.ch/ueberuns/rechtliche-hinweise/datenschutzbestimmungen-detail#rechte" target="_blank"
-	  rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.cash.ch/ueberuns/unternehmen" target="_blank"
-														rel="noopener">Impressum</a> von Cash</p>
-<p><a href="https://www.streaming.ch/datenschutz" target="_blank"
-	  rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.streaming.ch/impressum" target="_blank"
-														rel="noopener">Impressum</a> von Streaming</p>`),
+		'_POLICY_': prepareMacro(`<p>Wenn Sie der Datenbearbeitung basierend auf berechtigtem Interesse widersprechen oder ihre Einwilligung widerrufen wollen, können Sie dies in den entsprechenden Datenschutzbestimmungen in Kapitel 4.3 tun:</p>
+<p><a href="https://www.beobachter.ch/service/beobachter-und-guider-datenschutzbestimmungen" target="_blank" rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.beobachter.ch/service/beobachter-impressum" target="_blank" rel="noopener">Impressum</a> von Beobachter</p>
+<p><a href="https://www.handelszeitung.ch/datenschutz" target="_blank" rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.handelszeitung.ch/impressum" target="_blank" rel="noopener">Impressum</a> von HZ, Bilanz, Schweizer Versicherung</p>
+<p><a href="https://www.schweizer-illustrierte.ch/datenschutzbestimmungen-lang" target="_blank" rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.schweizer-illustrierte.ch/impressum" target="_blank" rel="noopener">Impressum</a> der Schweizer Illustrierten</p>
+<p><a href="https://www.cash.ch/ueberuns/rechtliche-hinweise/datenschutzbestimmungen-detail#rechte" target="_blank" rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.cash.ch/ueberuns/unternehmen" target="_blank" rel="noopener">Impressum</a> von Cash</p>
+<p><a href="https://www.streaming.ch/datenschutz" target="_blank" rel="noopener">Datenschutzbestimmungen</a> und <a href="https://www.streaming.ch/impressum" target="_blank" rel="noopener">Impressum</a> von Streaming</p>`),
 		'_PRIVACY_LINKS_': prepareMacro(`<p><a onclick="__tcfapi('showConsentDetailView', 2, function () {}, {tab: 1})">Datenschutz</a> | <a onclick="__tcfapi('showConsentDetailView', 2, function () {}, {tab: 1})">Impressum</a></p>`)
 	}
 };
